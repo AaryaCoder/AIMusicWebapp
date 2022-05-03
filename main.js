ea3d54d98a64e9ebeb04a3f0e2f3ae8879c7dd8a
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2022-05-03 18:37:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+scoreLeftWrist=0;
+song1_status=0;
+song2_status=0;
 
 function preload(){
     song1= loadSound("music.mp3");
@@ -21,6 +24,21 @@ function setup(){
 }
 function draw(){
     image(video,0,0,600,500);
+
+        song1_status=song1.isPlaying();
+        song2_status=song2.isPlaying();
+
+        if(scoreLeftWrist>0.2){
+            circle(leftWristX,leftWristY,20);
+            song2.stop();
+
+            if(song2_status=false){
+                song1.play();
+                document.getElementById("song").innerHTML="Playing the Harry Potter Theme Song!";
+            }
+        }
+    fill("#c963ff");
+    stroke("#c963ff");
 }
 function modelLoaded(){
     console.log("PoseNet has been initialized!");
@@ -28,6 +46,7 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
 
         leftWristX= results[0].pose.leftWrist.x;
         leftWristY= results[0].pose.leftWrist.y;
